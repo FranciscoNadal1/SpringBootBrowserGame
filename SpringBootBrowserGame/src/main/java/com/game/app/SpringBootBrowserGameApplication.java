b15f653804f9ddf0d7c1ab976221d6b94a654ae5
Francisco Nadal <francisconadal1@gmail.com>
2021-03-24 23:28:36 +0100

    start

---
+++ SpringBootBrowserGame/src/main/java/com/game/app/SpringBootBrowserGameApplication.java
@@ -0,0 +1,13 @@
+package com.game.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBrowserGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBrowserGameApplication.class, args);
+	}
+
+}
---
+++ SpringBootBrowserGame/src/test/java/com/game/app/SpringBootBrowserGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBrowserGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
